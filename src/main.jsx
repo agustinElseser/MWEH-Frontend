49d37c87697a0374673907f394b3a69e9724eeda
Agustin Elseser <agustin-elseser@hotmail.ocm>
2023-03-11 13:37:13 -0300

    REact

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ import "./slider.css";
 // const container = document.getElementById("root");
 // const root = hydrateRoot(container, <App tab="home" />);
 
-import { createRoot } from "react-dom/client";
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
