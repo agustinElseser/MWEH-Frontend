aa491dc912abe314ad10df0e227ab2f028446d41
Agustin Elseser <agustin-elseser@hotmail.ocm>
2023-03-11 13:48:25 -0300

    Vite Config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [react()],
   build: {
     outDir: "dist",
     emptyOutDir: true,
